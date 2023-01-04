6fc607b30be4aa1e60f579a884e94061d565a11c
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-01-04 18:00:59 -0300

    Comment Component in developmnt

---
+++ 01-fundamentos-reactjs/src/components/Comment/Comment.jsx
@@ -0,0 +1,33 @@
+import styles from './Comment.module.css'
+
+import { Trash, ThumbsUp } from 'phosphor-react'
+
+export function Comment() {
+
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/ViniciusdePSouza.png" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Vinícius de Paula</strong>
+                            <time title="04 de Janeiro de 2023" dateTime='2023-01-04 16:36:00'>Cerca de 1 hora atrás</time>
+                        </div>
+
+                        <button title="Excluir comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns !</p>
+                </div>
+                <footer>
+                    <button>
+                    <ThumbsUp size={20} />
+                    Aplaudir <span>02</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- 01-fundamentos-reactjs/src/components/Post/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post/Post.jsx
@@ -1,5 +1,7 @@
 import styles from './Post.module.css'
 
+import { Comment } from '../Comment/Comment'
+
 export function Post() {
     return (
         <article className={styles.post}>
@@ -32,6 +34,12 @@ export function Post() {
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
         </article>
     )
 }
